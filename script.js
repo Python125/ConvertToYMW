d145a65283f47684a5fdf3eb58fa929b31038c40
Python125 <91922582+Python125@users.noreply.github.com>
2022-06-17 09:44:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function daysToWMY(days) {
   const numOfDays = days;
   const year = Math.round(numOfDays / 365);
-  const month = Math.round(numOfDays / 30.417) % 12;
-  const week = Math.round(numOfDays / 7) % 52;
+  const month = Math.round(numOfDays % 365 / 30);
+  const week = Math.round(numOfDays % 365 % 30);
   
   
   return year + ' years: ' + month + ' months: ' + week + ' weeks';
 }
 
-console.log(daysToWMY(365));
+console.log(daysToWMY());
